ed9a625d2e73ee84983e9d63334fd42b2aed0616
Edgar Sánchez Grajeda <EdSG@users.noreply.github.com>
2022-05-09 21:58:14 -0700

    Time: 11 ms (25.07%), Space: 43.9 MB (57.37%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,35 +1,23 @@
 class Solution {
     
-    private static final String ADD = "+";
-    private static final String SUBSTRACT = "-";
-    private static final String MULTIPLY = "*";
-    private static final String DIVIDE = "/";
+    private static Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>();
+    
+    static {
+        OPERATIONS.put("+", (a, b) -> a + b);
+        OPERATIONS.put("-", (a, b) -> a - b);
+        OPERATIONS.put("*", (a, b) -> a * b);
+        OPERATIONS.put("/", (a, b) -> a / b);
+    }
     
     public int evalRPN(String[] tokens) {
         Stack<Integer> operands = new Stack<Integer>();
         
         Integer operand1, operand2;
         for (String token: tokens) {
-            if (token.equals(ADD)) {
+            if (OPERATIONS.containsKey(token)) {
                 operand2 = operands.pop();
                 operand1 = operands.pop();
-                
-                operands.push(operand1 + operand2);
-            } else if (token.equals(SUBSTRACT)) {
-                operand2 = operands.pop();
-                operand1 = operands.pop();
-                
-                operands.push(operand1 - operand2);
-            } else if (token.equals(MULTIPLY)) {
-                operand2 = operands.pop();
-                operand1 = operands.pop();
-                
-                operands.push(operand1 * operand2);
-            } else if (token.equals(DIVIDE)) {
-                operand2 = operands.pop();
-                operand1 = operands.pop();
-                
-                operands.push(operand1 / operand2);
+                operands.push(OPERATIONS.get(token).apply(operand1, operand2));
             } else {
                 operands.push(Integer.valueOf(token));
             }
